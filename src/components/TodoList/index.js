8ded84e7da8c3e97be92ddcdb4764dd0c316a933
toan_nguyen <nguyenduytoanbkdn@gmail.com>
2023-05-09 13:00:53 +0700

    test

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -4,14 +4,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { addTodo } from "../../redux/actions";
 import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
-import { todoListSelector } from "../../redux/selectors";
+import { todoListSelector, todoRemainingSelector } from "../../redux/selectors";
 
 export default function TodoList() {
   const [todoName, setTodoName] = useState();
   const [priority, setPriority] = useState("Medium");
 
   const dispatch = useDispatch();
-  const todoList = useSelector(todoListSelector);
+  const todoList = useSelector(todoRemainingSelector);
 
   const handleAddButtonClick = () => {
     dispatch(
